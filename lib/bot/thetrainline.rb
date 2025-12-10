69fe03f9996829accc28095f219d2e4c7ef9e5ac
Rohith <rohith.r.krish@gmail.com>
2025-12-11 00:46:24 +0100

    feat(api): add facade Bot::Thetrainline.find

--- lib/bot/thetrainline.rb
+++ lib/bot/thetrainline.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
+require_relative "../clients/file_client"
+require_relative "../journey_search"
 module Bot
   module Thetrainline
     module_function
 
-    def find(from, to, departure_at, options = {}); end
+    def find(from, to, departure_at, options = {})
+      client = options[:client] || Clients::FileClient.new(file_path: options[:data_file])
+      service = JourneySearch.new(from: from, to: to, departure_at: departure_at, client: client)
+      service.call
+    end
   end
 end
