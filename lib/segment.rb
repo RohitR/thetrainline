06d6782b54b0be59074e3a8fb3b6513cbfc7094e
Rohith <rohith.r.krish@gmail.com>
2025-12-10 23:58:49 +0100

    feat(domain): add immutable Segment and Fare value objects

---
+++ lib/segment.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "date"
+
+class Segment
+  attr_reader :departure_station, :departure_at, :arrival_station, :arrival_at,
+              :service_agencies, :duration_in_minutes, :changeovers, :products, :fares
+
+  def initialize(departure_station:, departure_at:, arrival_station:, arrival_at:,
+                 service_agencies:, duration_in_minutes:, changeovers:, products:, fares:)
+    @departure_station = departure_station
+    @departure_at = to_datetime(departure_at)
+    @arrival_station = arrival_station
+    @arrival_at = to_datetime(arrival_at)
+    @service_agencies = Array(service_agencies)
+    @duration_in_minutes = duration_in_minutes.to_i
+    @changeovers = changeovers.to_i
+    @products = Array(products)
+    @fares = Array(fares).freeze
+    freeze
+  end
+
+  def to_h
+    {
+      departure_station: departure_station,
+      departure_at: departure_at,
+      arrival_station: arrival_station,
+      arrival_at: arrival_at,
+      service_agencies: service_agencies,
+      duration_in_minutes: duration_in_minutes,
+      changeovers: changeovers,
+      products: products,
+      fares: fares.map { |f| f.to_h }
+    }
+  end
+
+  private
+
+  def to_datetime(val)
+    case val
+    when String then DateTime.parse(val)
+    when Time then val.to_datetime
+    when DateTime then val
+    else val
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "bundler/setup"
+require "rspec"
+require "date"
+require_relative "../lib/segment"
+require_relative "../lib/fare"
